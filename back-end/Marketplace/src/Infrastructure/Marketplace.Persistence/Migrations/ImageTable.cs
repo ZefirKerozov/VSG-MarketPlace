02779169095db42e972bd7f3cc25e25a6611f3aa
ZefirKerozov <ZKerozov@vsgbg.com>
2023-04-25 18:01:11 +0300

    Add fluent migration

---
+++ back-end/Marketplace/src/Infrastructure/Marketplace.Persistence/Migrations/ImageTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Marketplace.Persistence.Migrations;
+
+[Migration(20230525)]
+
+public class ImageTable :Migration
+{
+    public override void Up()
+    {
+        Create.Table("Image")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity().NotNullable()
+            .WithColumn("img").AsString(50).NotNullable();
+
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Image");
+    }
+}
---
+++ back-end/Marketplace/src/Infrastructure/Marketplace.Persistence/Migrations/ProductTable.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace Marketplace.Persistence.Migrations;
+
+[Migration(20230525)]
+public class ProductTable :Migration
+{
+    public override void Up()
+    {
+        Create.Table("Product")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity().NotNullable()
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Code").AsString(255).NotNullable()
+            .WithColumn("Quantity").AsInt64().NotNullable()
+            .WithColumn("Description").AsString().NotNullable()
+            .WithColumn("QuantityForSale").AsInt64().NotNullable()
+            .WithColumn("CategoryId").AsInt64().NotNullable().ForeignKey("Categories", "Id")
+            .WithColumn("Location").AsString().NotNullable()
+            .WithColumn("Price").AsDecimal().NotNullable();
+
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Product");
+    }
+}
