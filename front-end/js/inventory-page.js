9961858debda86456d7b67aef85487d40cb48c8b
Yasen Kurtev <YKurtev@VSG.COM>
2023-04-25 14:59:10 +0300

    Created separate js file for dark mode functionality

--- front-end/js/inventory-page.js
+++ front-end/js/inventory-page.js
@@ -1,55 +1,9 @@
 import { makeRequest } from "../utils/makeRequest.js";
 import "../utils/navLinks.js";
 import "../utils/hamburgerMenu.js";
+import "../utils/darkMode.js";
 import { addItem, deleteItem, getAllCategories, getAllProducts, modifyItem } from "../utils/requests.js";
 
-// Dark mode functionality
-
-document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-
-const darkModeSwitchHeader = document.querySelector('#dark-mode-header');
-const darkModeSwitchAside = document.querySelector('#dark-mode-aside');
-
-const theme = localStorage.getItem('theme');
-
-if (theme === undefined) {
-    localStorage.setItem('theme', 'light');
-}
-
-if (theme === 'dark') {
-    darkModeSwitchHeader.checked = true;
-    darkModeSwitchAside.checked = true;
-} else if (theme === 'light') {
-    darkModeSwitchHeader.checked = false;
-    darkModeSwitchAside.checked = false;
-}
-
-darkModeSwitchHeader.addEventListener('change', onDarkModeSwitchHeader);
-darkModeSwitchAside.addEventListener('change', onDarkModeSwitchAside);
-
-function onDarkModeSwitchHeader() {
-    if (darkModeSwitchHeader.checked) {
-        console.log('checked');
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    } else {
-        console.log('unchekced');
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    }
-};
-
-function onDarkModeSwitchAside() {
-    if (darkModeSwitchAside.checked) {
-        console.log('checked');
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    } else {
-        console.log('unchekced');
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    }
-};
 
 // Open add item modal if add item button is clicked
 
--- front-end/js/pending-orders-page.js
+++ front-end/js/pending-orders-page.js
@@ -1,56 +1,9 @@
 import "../utils/navLinks.js";
 import "../utils/hamburgerMenu.js";
+import "../utils/darkMode.js";
 import "../components/pending-orders-item.js";
 import { makeRequest } from "../utils/makeRequest.js";
 import { completeOrder, getAllPendingOrders } from "../utils/requests.js";
-
-// Dark mode functionality
-
-document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-
-const darkModeSwitchHeader = document.querySelector('#dark-mode-header');
-const darkModeSwitchAside = document.querySelector('#dark-mode-aside');
-
-const theme = localStorage.getItem('theme');
-
-if (theme === undefined) {
-    localStorage.setItem('theme', 'light');
-}
-
-if (theme === 'dark') {
-    darkModeSwitchHeader.checked = true;
-    darkModeSwitchAside.checked = true;
-} else if (theme === 'light') {
-    darkModeSwitchHeader.checked = false;
-    darkModeSwitchAside.checked = false;
-}
-
-darkModeSwitchHeader.addEventListener('change', onDarkModeSwitchHeader);
-darkModeSwitchAside.addEventListener('change', onDarkModeSwitchAside);
-
-function onDarkModeSwitchHeader() {
-    if (darkModeSwitchHeader.checked) {
-        console.log('checked');
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    } else {
-        console.log('unchekced');
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    }
-};
-
-function onDarkModeSwitchAside() {
-    if (darkModeSwitchAside.checked) {
-        console.log('checked');
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    } else {
-        console.log('unchekced');
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
-    }
-};
 
 const DUMMY_DATA = [
     {
